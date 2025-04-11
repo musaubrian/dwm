615c3ff744088a60d75b0b601026c418b9ac8fd0
musaubrian <musaubrian15@gmail.com>
2025-04-11 12:41:21 +0300

    patched up all nice

--- config.def.h
+++ config.def.h
@@ -1,21 +1,29 @@
 /* See LICENSE file for copyright and license details. */
+#include <X11/XF86keysym.h>
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+static const char *fonts[]          = { "Iosevka:size=11" };
+
+static const char col_bg[]       = "#1d2021"; // base bg
+static const char col_bg_focus[] = "#3c3836"; // focused bg
+static const char col_fg[]       = "#fbf1c7"; // bright text
+static const char col_fg_inactive[] = "#a89984"; // faded text
+static const char col_border_focus[] = "#3c3836";
+static const char col_border_inactive[] = "#282828";
+
+static const char *colors[][3] = {
+	/*               fg              bg             border */
+	[SchemeNorm] = { col_fg_inactive, col_bg,         col_border_inactive },
+	[SchemeSel]  = { col_fg,          col_bg_focus,   col_border_focus },
 };
 
 /* tagging */
@@ -27,8 +35,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Chromium",  NULL,       NULL,       1 << 2,       0,           -1 },
+	// { "Gimp",     NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
@@ -40,12 +48,12 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[f]",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -57,14 +65,19 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *launcherCmd[] = {"rofi", "-show", "drun", NULL};
 static const char *termcmd[]  = { "st", NULL };
+static const char *browserCmd[] = {"chromium", NULL};
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_p,      spawn,          {.v = launcherCmd } },
+	{ MODKEY,                       XK_t,      spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_b,      spawn,          {.v = browserCmd } },
+	{ MODKEY|ShiftMask,             XK_d,      spawn,          SHCMD("xrandr --output HDMI-1 --auto --right-of eDP-1") },
+	{ MODKEY|ShiftMask,             XK_x,      spawn,          SHCMD("xrandr --auto") },
+
+	{ MODKEY|ShiftMask,             XK_f,      fullscreen,     {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
@@ -74,8 +87,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
@@ -85,6 +97,14 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	//
+	{ 0,   XF86XK_AudioRaiseVolume,    spawn,     {.v = (const char*[]){"wpctl", "set-volume", "@DEFAULT_SINK@", ".1+", NULL} } },
+	{ 0,   XF86XK_AudioLowerVolume,    spawn,     {.v = (const char*[]){"wpctl", "set-volume", "@DEFAULT_SINK@", ".1-", NULL} } },
+	{ 0,   XF86XK_AudioMute,           spawn,     {.v = (const char*[]){"wpctl", "set-mute", "@DEFAULT_SINK@", "toggle", NULL} } },
+	{ 0,   XF86XK_AudioMicMute,        spawn,     {.v = (const char*[]){"wpctl", "set-mute", "@DEFAULT_SOURCE@", "toggle", NULL} } },
+	{ 0,   XF86XK_MonBrightnessDown,   spawn,     {.v = (const char*[]){"brightnessctl", "s", "1-%", NULL} } },
+	{ 0,   XF86XK_MonBrightnessUp,     spawn,     {.v = (const char*[]){"brightnessctl", "s", "+1%", NULL} } },
+	{ 0,   XK_Print,                   spawn,     {.v = (const char*[]){"flameshot", "gui", NULL} } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
