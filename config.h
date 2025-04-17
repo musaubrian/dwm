de2b17d51b0338bdd842c6aa6021012655f8374c
musaubrian <musaubrian15@gmail.com>
2025-04-17 19:19:48 +0300

    proper layout for monocle mode

--- config.h
+++ config.h
@@ -50,7 +50,7 @@ static const Layout layouts[] = {
 	{ "[\\]",      dwindle },
 	{ "[]=",      tile },
 	{ "[f]",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "[M]",      monocle }, // Kinda fullscreen-ish
 	{ "[@]",      spiral },
 };
 
@@ -90,7 +90,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
